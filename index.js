00ffb6c6241d7ee85806d78683a8e42d649eac84
mwigand32 <mwigand32@yahoo.com>
2023-07-16 12:03:42 -0700

    Initial function created to build a 16x16 square grid onto the page, individual tiles go from grey to black when mouse'd over.
    Script functions well in its first phase to allow the user to draw on the grid when hovering over any tiles.

---
+++ index.js
@@ -0,0 +1,18 @@
+function buildGrid() {
+    const container = document.querySelector('.container');
+    let squareSize = 16;
+
+    for(let i = 0; i < squareSize; i++){
+        for(let j = 0; j < squareSize; j++){
+            const square = document.createElement('div');
+            square.setAttribute('style', 'width: 6.25%; background-color: #808080')
+            container.appendChild(square);
+
+            square.addEventListener("mouseover", (e) => {
+                square.style.backgroundColor = '#000'
+            })
+        }
+    }
+}
+
+buildGrid();
